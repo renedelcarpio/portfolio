76699a86050bd50b5ab4544165a32fed70d1251b
Rene Del Carpio <delcarpio.rene@gmail.com>
2021-07-14 21:41:18 -0400

    Components were created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import Layout from './components/Layout';
+import Hero from './components/Hero';
+import Main from './components/Main';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Experience from './components/Experience';
 
 const App = () => {
 	return (
-		<div>
-			<h1>hello me!</h1>
-		</div>
+		<Layout>
+			<Hero />
+			<Main />
+			<Experience />
+			<Projects />
+			<Contact />
+		</Layout>
 	);
 };
 
