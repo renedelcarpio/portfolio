f7cc3ed9aff9572767d160afe15d5febe0af4c4c
Rene Del Carpio <delcarpio.rene@gmail.com>
2022-01-30 00:04:25 -0400

    add components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import About from './components/Pages/About/About';
 import Main from './components/Pages/Main/Main';
 import Projects from './components/Pages/Projects/Projects';
+import Skills from './components/Pages/Skills/Skills';
 
 const App = () => {
 	return (
 		<>
 			<Main />
 			<About />
+			<Skills />
 			<Projects />
 		</>
 	);
