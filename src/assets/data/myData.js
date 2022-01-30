6ce6a904221b4b810b715099c08bcbee729a217a
Rene Del Carpio <delcarpio.rene@gmail.com>
2022-01-30 15:33:16 -0400

    create my data array

---
+++ src/assets/data/myData.js
@@ -0,0 +1,22 @@
+export const myData = [
+	{
+		name: 'Email',
+		logo: 'far fa-envelope',
+		url: 'delcarpio.rene@gmail.com',
+	},
+	{
+		name: 'LinkedIn',
+		logo: 'fab fa-linkedin-in',
+		url: 'https://www.linkedin.com/in/rene-del-carpio-5b624992/',
+	},
+	{
+		name: 'GitHub',
+		logo: 'fab fa-github',
+		url: 'https://github.com/renedelcarpio',
+	},
+	{
+		name: 'Phone',
+		logo: 'fas fa-mobile-alt',
+		url: '+591 798 80310',
+	},
+];
