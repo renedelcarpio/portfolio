48f8d53311f34b5bdeea230f6fb415af1cf5c665
Rene Del Carpio <delcarpio.rene@gmail.com>
2022-01-30 15:32:54 -0400

    create my projects array

---
+++ src/assets/data/myProjects.js
@@ -0,0 +1,39 @@
+import calcD from '../images/calcD.png';
+import calcM from '../images/calcM.png';
+import calleD from '../images/calleD.png';
+import calleM from '../images/calleM.png';
+import esVendiendoD from '../images/esVendiendoD.png';
+import esVendiendoM from '../images/esVendiendoM.png';
+import expenseD from '../images/expenseDesktop.png';
+import expenseM from '../images/expenseMobile.png';
+
+export const myPages = [
+	{
+		name: 'Expense Control',
+		url: 'https://app-gastos-95f7b.web.app',
+		description: 'Some description must be here',
+		desktop: expenseD,
+		mobile: expenseM,
+	},
+	{
+		name: 'Calculator',
+		url: 'https://renedelcarpio.github.io/calculator/',
+		description: 'Some description must be here',
+		desktop: calcD,
+		mobile: calcM,
+	},
+	{
+		name: 'Es Vendiendo',
+		url: 'https://esvendiendo.com/',
+		description: 'Some description must be here',
+		desktop: esVendiendoD,
+		mobile: esVendiendoM,
+	},
+	{
+		name: 'Calle Performance',
+		url: 'https://calleperformance.art/',
+		description: 'Some description must be here',
+		desktop: calleD,
+		mobile: calleM,
+	},
+];
