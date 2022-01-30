91804974325304af84bfecc70ca58800453bd83f
Rene Del Carpio <delcarpio.rene@gmail.com>
2022-01-30 15:34:42 -0400

    create projects component and styles

--- src/components/Pages/Projects/Projects.js
+++ src/components/Pages/Projects/Projects.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import './Projects.scss';
+
+import ProjectCard from '../../common/ProjectCard/ProjectCard';
+import { myPages } from '../../../assets/data/myProjects';
 
 const Projects = () => {
 	return (
-		<div>
-			<h1>Projects</h1>
+		<div id='projects' className='projects'>
+			<div className='projects__title'>
+				<h4 className='about__discover'>What I Do</h4>
+				<h1 className='about__title'>My Projects</h1>
+				<hr size='2px' width='7%' color='#f45659' />
+			</div>
+			<div>
+				{myPages.map(({ name, url, description, desktop, mobile, index }) => {
+					return (
+						<ProjectCard
+							name={name}
+							url={url}
+							description={description}
+							desktop={desktop}
+							mobile={mobile}
+							key={`${index}${name}`}
+						/>
+					);
+				})}
+			</div>
 		</div>
 	);
 };
