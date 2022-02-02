83a44e6cdb7117c6b0f4b8ec961961746f494d62
Rene Del Carpio <delcarpio.rene@gmail.com>
2022-02-01 23:17:44 -0400

    refactor navbar to work responsive

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,38 +1,54 @@
 import './Navbar.scss';
 
+import { useState } from 'react';
+
 const Navbar = () => {
+	const [isMobile, setIsMobile] = useState(true);
+
 	return (
 		<div className='navbar'>
-			<h2 className='navbar__name'>Rene Del Carpio</h2>
-			<i className='fas fa-ellipsis-h'></i>
-			<ul className='navbar__list'>
-				<i className='fas fa-times'></i>
-				<li>
-					<a className='navbar__item' href='#home'>
+			<h1 className='navbar__name'>Rene Del Carpio</h1>
+
+			<ul
+				className={isMobile ? 'navbar__list--mobile' : 'navbar__list'}
+				onClick={() => setIsMobile(false)}
+			>
+				<li className='navbar__item'>
+					<a className='navbar__link' href='#home'>
 						Home
 					</a>
 				</li>
-				<li>
-					<a className='navbar__item' href='#about'>
+				<li className='navbar__item'>
+					<a className='navbar__link' href='#about'>
 						About
 					</a>
 				</li>
-				<li>
-					<a className='navbar__item' href='#skills'>
+				<li className='navbar__item'>
+					<a className='navbar__link' href='#skills'>
 						Skills
 					</a>
 				</li>
-				<li>
-					<a className='navbar__item' href='#projects'>
+				<li className='navbar__item'>
+					<a className='navbar__link' href='#projects'>
 						Projects
 					</a>
 				</li>
-				<li>
-					<a className='navbar__item' href='#contact'>
+				<li className='navbar__item'>
+					<a className='navbar__link' href='#contact'>
 						Contact
 					</a>
 				</li>
 			</ul>
+			<button
+				className='navbar__mobile--icon'
+				onClick={() => setIsMobile(!isMobile)}
+			>
+				{isMobile ? (
+					<i className='fas fa-times'></i>
+				) : (
+					<i className='fas fa-bars'></i>
+				)}
+			</button>
 		</div>
 	);
 };
