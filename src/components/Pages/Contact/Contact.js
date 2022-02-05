ab3254116f209f6c00eb605590a8b493d67f1b92
Rene Del Carpio <delcarpio.rene@gmail.com>
2022-02-05 10:53:29 -0400

    add key to contact component

--- src/components/Pages/Contact/Contact.js
+++ src/components/Pages/Contact/Contact.js
@@ -11,9 +11,15 @@ const Contact = () => {
 				<h1 className='about__title'>Contact me</h1>
 				<hr size='2px' width='7%' color='#f45659' />
 			</div>
+			<p className='contact__message'>
+				If you have any questions about my services, or just want to say hello,
+				feel free to contact me. Below are my social pages and my email:
+			</p>
 			<div className='contact__data'>
-				{myData.map(({ logo, url }) => {
-					return <ContactComponent logo={logo} url={url} />;
+				{myData.map(({ logo, url, index, name }) => {
+					return (
+						<ContactComponent key={`${name}${index}`} logo={logo} url={url} />
+					);
 				})}
 			</div>
 		</div>
