9c3a9fdcccca6fa7744c04d84fce9004e6ec49f0
Rene Del Carpio <delcarpio.rene@gmail.com>
2022-02-05 10:42:08 -0400

    write about me and funtionality to download cv

--- src/components/Pages/About/About.js
+++ src/components/Pages/About/About.js
@@ -2,8 +2,16 @@ import './About.scss';
 
 import Button from '../../common/Button/Button';
 import Photo from '../../../assets/images/photo2.jpeg';
+import cv from '../../../assets/images/ReneDelCarpioCV.pdf';
+import { saveAs } from 'file-saver';
 
 const About = () => {
+	const handleClick = () => {
+		saveAs(
+			'https://drive.google.com/drive/folders/12m5-82K9KpCPJgToMb6KH_oVIXLwkBSN?usp=sharing',
+			'ReneDelCarpioCV.pdf'
+		);
+	};
 	return (
 		<div id='about' className='about'>
 			<img className='about__image' src={Photo} alt='' />
@@ -12,17 +20,17 @@ const About = () => {
 				<h1 className='about__title'>About me</h1>
 				<hr size='2px' width='7%' color='#f45659' />
 				<p className='about__paragraph'>
-					Hi, I'm Rene, a frontend developer with a great passion for
-					motorcycles and technology. I like to challenge myself so I am
-					constantly learning. I firmly believe that perseverance is the way to
-					success and that is why I never give up. I really enjoy seeing the
-					designs come true on the screen with a few lines of code, my favorite
-					language is JavaScript with the React.js library. Feel free to contact
-					me if you think I can add value to you or your team or you just think
-					we could be friends.
+					Hi, I’m Rene, I’m a software developer with big passion for
+					motorcycles and tech. I love to develop high level responsive web
+					applications. I’m quietly confident, naturally curious and I have a
+					great determination to overcome any obstacle. I’m always learning and
+					I continue challenging me to build the best work possible. I’m seeking
+					out opportunities to collaborate with companies or agencies where egos
+					are out of the equation. If that sounds good to you don’t hesitate to
+					contact me.
 				</p>
 				<div className='about__button'>
-					<Button>Resume</Button>
+					<Button onClick={handleClick}>Resume</Button>
 				</div>
 			</div>
 		</div>
